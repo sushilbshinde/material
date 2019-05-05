04894a94acda96fc08f9d3da8f71ead0c877a8d0
Sushi Shinde <sushilbshinde@gmail.com>
2019-05-05 21:20:05 +0530

    date picker

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
+import { OrderNewComponent } from './order-new/order-new.component';
 
 // Material components
+import { MatButtonModule } from '@angular/material/Button';
+import { MatCardModule } from '@angular/material/Card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderNewComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     OrdersRoutingModule,
-    MatTabsModule
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    MatMomentDateModule
   ]
 })
 export class OrdersModule { }
