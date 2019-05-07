2af2022b448f0e91cf40439024259fdf144d0941
Sushi Shinde <sushilbshinde@gmail.com>
2019-05-07 09:52:28 +0530

    Tables, part 1

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -1,4 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+
+const ELEMENT_DATA = [
+  {
+    orderDate: new Date(),
+    orderNumber: 100,
+    total: 29.99,
+    description:'2lbs of tuna',
+    isChecked: false
+  },
+  {
+    orderDate: new Date(),
+    orderNumber: 101,
+    total: 39.99,
+    description:'5lbs of tuna',
+    isChecked: false
+  },
+  {
+    orderDate: new Date(),
+    orderNumber: 102,
+    total: 59.99,
+    description:'1lbs of tuna',
+    isChecked: false
+  },
+];
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +31,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
+  displayedColumns:string[] = ['action','orderNumber','orderDate','description','total'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort:MatSort;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -9,9 +9,12 @@ import { OrderNewComponent } from './order-new/order-new.component';
 // Material components
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
@@ -24,9 +27,12 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
     OrdersRoutingModule,
     MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatIconModule,
     MatInputModule,
+    MatSortModule,
+    MatTableModule,
     MatTabsModule,
     MatMomentDateModule
   ]
