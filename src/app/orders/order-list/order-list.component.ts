42c9db2c3fd648bc1fb35c3fe9496ad884ce1933
Sushi Shinde <sushilbshinde@gmail.com>
2019-05-07 11:27:23 +0530

    Pagination

--- src/app/orders/order-list/order-list.component.ts
+++ src/app/orders/order-list/order-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, MatTableDataSource } from '@angular/material';
+import { PageEvent } from '@angular/material';
 
 const ELEMENT_DATA = [
   {
@@ -74,13 +75,31 @@ const ELEMENT_DATA = [
 })
 export class OrderListComponent implements OnInit {
   displayedColumns:string[] = ['action','orderNumber','orderDate','description','total'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource:MatTableDataSource<object>;
+
+  length = 100;
+  pageIndex = 0;
+  pageSize = 10;
+  pageSizeOptions = [1,2,5,10];
 
   @ViewChild(MatSort) sort:MatSort;
 
+  pageEvent:PageEvent;
+
   constructor() { }
 
+  onPageChange(e) {
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.loadData(this.pageIndex, this.pageSize);
+  }
+
+  loadData(pageIndex, pageSize) {
+    this.dataSource = new MatTableDataSource<object>(ELEMENT_DATA.slice(pageIndex, pageIndex + pageSize))
+  }
+
   ngOnInit() {
+    this.loadData(0, this.pageSize)
     this.dataSource.sort = this.sort;
   }
 
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -13,6 +13,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -31,6 +32,7 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
     MatDatepickerModule,
     MatIconModule,
     MatInputModule,
+    MatPaginatorModule,
     MatSortModule,
     MatTableModule,
     MatTabsModule,
